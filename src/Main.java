c4028a9ebc0e52e964b05a98f95395e052fb7143
Mwai <machariamwai@gmail.com>
2023-01-11 16:19:09 +0300

    A getter and a setter walk into a bar...

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        Phone iPhone = new Phone("iPhone 12", 8, 8, 24);
+//        iPhone.setName("iPhone 12");
+
+//        iPhone.setMemoryRam(8);
+
+        System.out.println(iPhone.getName());
+//        System.out.println(iPhone.screenSize);
+
+        iPhone.playMusic("Draconian");
+        iPhone.playMusic("Chris Brown");
+
+        Phone pixel = new Phone("Pixel 3");
+
+    }
+}
